7794ab8ccf11098f1ad71fd9f02272671ee8378d
ibrahimW <35659676+ibrahimW@users.noreply.github.com>
2019-01-02 23:37:08 +0300

    Created env variable for API key

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
 const request = require('request');
-
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia&key=AIzaSyDQkcL6XAkC1M2u0IQpCS7MZDC71cpvatc',
+  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia' + '&key=' + process.env.API_KEY,
   json: true
 }, (error, response, body) => {
-  console.log(body);
+    console.log(JSON.stringify(body, undefined, 2));
 });
