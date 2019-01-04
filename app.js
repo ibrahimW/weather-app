936369edbd1b3cff2a3b03a51b744a8a09e47435
ibrahimW <35659676+ibrahimW@users.noreply.github.com>
2019-01-04 14:13:00 +0300

    Error handling + encoding address for URL

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
 require('dotenv').config();
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+ .options({
+   a: {
+     demand: true,
+     alias: 'address',
+     describe: 'address to fetch weather for',
+     string: true
+   }
+ })
+ .help()
+ .alias('help', 'h')
+ .argv;
+
+var encodedAddress = encodeURIComponent(argv.address);
+
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia' + '&key=' + process.env.API_KEY,
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}` + '&key=' + process.env.API_KEY,
   json: true
 }, (error, response, body) => {
-    console.log(JSON.stringify(body, undefined, 2));
+  if (error) {
+    console.log('Unable to connect to Google servers.');
+  } else if (body.status === 'ZERO_RESULTS') {
+    console.log('Unable to find that address.');
+  } else if (body.status === 'OK'){
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  }
 });
