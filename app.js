b81be31b64ad9f829dc05aee1437cdcf406299ce
ibrahimW <35659676+ibrahimW@users.noreply.github.com>
2019-01-04 14:39:30 +0300

    refactoring( moved function to seperate file)

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-require('dotenv').config();
-const request = require('request');
 const yargs = require('yargs');
+const geocode = require('./geocode/geocode');
 
 const argv = yargs
  .options({
@@ -15,19 +14,10 @@ const argv = yargs
  .alias('help', 'h')
  .argv;
 
-var encodedAddress = encodeURIComponent(argv.address);
-
-request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}` + '&key=' + process.env.API_KEY,
-  json: true
-}, (error, response, body) => {
-  if (error) {
-    console.log('Unable to connect to Google servers.');
-  } else if (body.status === 'ZERO_RESULTS') {
-    console.log('Unable to find that address.');
-  } else if (body.status === 'OK'){
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  if (errorMessage) {
+    console.log(errorMessage);
+  } else {
+    console.log(JSON.stringify(results, undefined, 2));
   }
 });
