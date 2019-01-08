3d4248ac3ce45999aa33c58efb4f17066160344d
A A Karim <adil.karim@pivigo.com>
2019-01-08 19:29:17 +0100

    Create basic Http server version

---
+++ server.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const http = require('http');
+const url = require('url');
+const geocode = require('./geocode/geocode');
+const weather = require('./weather/weather');
+
+const PORT = process.env.PORT;
+
+http.createServer(function (req, res) {
+  const query = url.parse(req.url, true).query;
+  console.log(`Received query address ${query.address}`);
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  geocode.geocodeAddress(query.address, (errorMessage, results) => {
+    if (errorMessage) {
+      console.log(errorMessage);
+    } else {
+      res.write(results.address);
+      weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {
+        if (errorMessage)  {
+          console.log(errorMessage);
+        } else {
+          res.write(`Its currently ${weatherResults.temperature}. It feels like ${weatherResults.apparentTemperature}.`);
+          res.end();
+        }
+      });
+    }
+  });
+
+}).listen(PORT);
