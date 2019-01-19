ad14057e0b7186dd96a2a1f1049df751458c338a
ibrahimW <35659676+ibrahimW@users.noreply.github.com>
2019-01-19 18:58:25 +0300

    Implemented Puppeteer

--- weather/weather.js
+++ weather/weather.js
@@ -1,21 +1,51 @@
-require('dotenv').config();
-const request = require('request');
+const puppeteer = require("puppeteer");
+const url = "https://darksky.net";
 
-var getWeather = (lat, lng, callback) => {
-  request({
-    url: `https://api.darksky.net/forecast/${process.env.WEATHER_API}/${lat},${lng}`,
-    json: true
-  }, (error, response, body) => {
-     if (!error && response.statusCode === 200) {
-       //console.log(body.currently.temperature);
-       callback(undefined, {
-         temperature: body.currently.temperature,
-         apparentTemperature: body.currently.apparentTemperature
-       });
-     } else {
-      callback('Unable to fetch Weather.');
-    }
-  });
+const getWeather = async (lat, lng, callback) => {
+  const browser = await puppeteer.launch();
+  try {
+    console.log(`Getting weather for ${lat} ${lng}...`);
+    const page = await browser.newPage();
+    await page.goto(url);
+    const inputSelector = '#searchForm > input[type="text"]';
+    // clear pre-set input
+    await page.click(inputSelector, { clickCount: 3 });
+    await page.keyboard.press("Backspace");
+    // enter text into input field
+    const inputText = `${lat} ${lng}`;
+    await page.type(inputSelector, inputText);
+    await page.screenshot({ path: "textInput.png" });
+
+    // click submit icon
+    // Wait until next page has loaded
+    console.log("Navigating to results page...");
+    await Promise.all([
+      page.screenshot({ path: "pageClick.png" }),
+      page.waitForNavigation({ waitUntil: "networkidle0" }), // The promise resolves after navigation has finished
+      page.click("#searchForm > a.searchButton")
+    ]);
+
+    await page.screenshot({ path: "results.png" });
+    // Get results
+    console.log("Getting results...");
+    const results = await page.evaluate(() => {
+      const temperature = document.querySelector(
+        "#title > span.currently > span.desc.swap > span.summary.swap"
+      ).textContent;
+      const apparentTemperature = document.querySelector(
+        "#title > span.currently > span.desc.swap > span.summary-high-low > span:nth-child(1)"
+      ).childNodes[1].textContent;
+      return {
+        temperature,
+        apparentTemperature
+      };
+    });
+    callback(null, results);
+  } catch (e) {
+    console.error(e);
+  } finally {
+    browser.close();
+  }
 };
 
 module.exports.getWeather = getWeather;
